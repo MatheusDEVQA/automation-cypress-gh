970df9c1dd82eced02d4b453b4c0af97800c1691
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-02-21 19:09:50 -0300

    Methods PUT and DELETE

--- cypress/e2e/ApiTesting/HttpRequest.cy.js
+++ cypress/e2e/ApiTesting/HttpRequest.cy.js
@@ -20,4 +20,29 @@ describe('HTTP Request Test', () => {
             .its('status')
             .should('equal', 201)
     })
+
+    it('PUT call', () =>{
+        cy.request({
+            method:'PUT',
+            url: 'https://jsonplaceholder.typicode.com/posts/',
+            body:
+            {
+                title: 'foo',
+                body: 'this put',
+                userId: 1,
+                id: 1
+            }
+
+        })
+        .its('status')
+        .should('equal', 200)
+    })
+
+    it('DELETE call', ()=>{
+        cy.request({
+            method: 'DELETE',
+            url: 'https://jsonplaceholder.typicode.com/posts/1',
+            
+        })
+    })
 })
