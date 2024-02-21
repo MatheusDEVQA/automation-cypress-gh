6e7e3f2f9f7b9eccda9afc83b3cf6f168ac5c98a
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-02-21 18:53:17 -0300

    Add post test api

--- cypress/e2e/ApiTesting/HttpRequest.cy.js
+++ cypress/e2e/ApiTesting/HttpRequest.cy.js
@@ -1,8 +1,23 @@
 describe('HTTP Request Test', () => {
 
-    it('Get call', ()=>{
+    it('Get call', () => {
         cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/1')
-        .its('status')
-        .should('equal', 200)
+            .its('status')
+            .should('equal', 200)
+    })
+
+    it('Post call', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://jsonplaceholder.typicode.com/posts/',
+            body:
+            {
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }
+        })
+            .its('status')
+            .should('equal', 201)
     })
 })
