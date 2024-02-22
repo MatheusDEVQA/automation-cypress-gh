800e9537fc9658cc346bb7ab7dc558ffa833e09f
MatheusDEVQA <102087318+MatheusDEVQA@users.noreply.github.com>
2024-02-22 14:26:46 -0300

    Add approach using object json

---
+++ cypress/e2e/ApiTesting/PostCalls.cy.js
@@ -0,0 +1,33 @@
+describe('Api testing Post', () => {
+
+    const requestBodyRandom = {
+    
+        name: Math.random().toString(5).substring(2),
+        email: Math.random().toString(6).substring(2) + '@gmail.com',
+        job: 'employer'
+
+    }
+    it('Approach - Codes json object', () => {
+        const requestBody =
+        {
+            name: 'matheus',
+            job: 'quality engineer'
+        }
+        cy.request({
+            method: 'POST',
+            url: 'https://reqres.in/api/users',
+            body: requestBody
+        })
+            .then((reponse) => {
+                expect(reponse.status).to.eq(201)
+                expect(reponse.body.name).to.eq(requestBody.name)
+                expect(reponse.body.job).to.eq(requestBody.job)
+
+
+            })
+    })
+
+    it('Approach using fixtures', ()=>{
+        
+    })
+})
