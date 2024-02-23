d8c6ac9419b336f8a3402f1d10c6b24e00c119ba
MatheusDEVQA <102087318+MatheusDEVQA@users.noreply.github.com>
2024-02-23 18:36:41 -0300

    Add test with query parameter

---
+++ cypress/e2e/ApiTesting/QueryParams.cy.js
@@ -0,0 +1,17 @@
+describe('API testtin with param', () =>{
+    it('Passing parameters', () =>{
+        cy.request({
+            method: 'GET',
+            url: 'https://reqres.in/api/users',
+            qs: 2,
+                   
+        })
+        .then((response) =>{
+            expect(reponse.status).equal(200)
+            expect(response.body.page).to.eq(2)
+            expect(response.body.data).has.length(6)
+            expect(response.body.data[2]).have.property('id', 9)
+            expect(response.body.data[0]).has.property('first_name','Michael')
+        })
+    })
+})
