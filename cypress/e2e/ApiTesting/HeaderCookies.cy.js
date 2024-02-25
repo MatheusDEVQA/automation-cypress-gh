347b65a6849183887618ca7b73d344bbc4d235e4
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-02-24 21:44:45 -0300

    Add test has hearders and cookies

--- cypress/e2e/ApiTesting/HeaderCookies.cy.js
+++ cypress/e2e/ApiTesting/HeaderCookies.cy.js
@@ -1,16 +1,58 @@
-describe('Api testing hearde and cookies', ()=>{
+
+describe('Api testing hearde and cookies', () => {
 
     let token
 
-    before('Create acceess token', ()=>{
+    before('Create acceess token', () => {
         cy.request({
             method: 'POST',
-            url: '',
-            hearders:{
-                
+            url: 'https://simple-books-api.glitch.me/api-clients/',
+            hearders: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                clientName: 'Matheus',
+                clientEmail: Math.random().toString(5).substring(2) + "@gmail.com"
             }
+        })
+        .then((response) => {
+            token = response.body.accessToken
         })
     })
 
-    it('')
+    it('Create new order', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://simple-books-api.glitch.me/orders',
+            hearders: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: {
+                "bookId": 1,
+                    "customerName": "Matheus"
+            }
+        })
+        .then((response) =>{
+            expect(response.status).to.eq(201)
+            expect(response.body.created).to.eq(true)
+        })
+    })
+    it('Fetching the orders', ()=>{
+        cy.request({
+            method: 'GET',
+            url: 'https://simple-books-api.glitch.me/orders/',
+            hearders:{
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+token
+            },
+            cookies:{
+                'cookieName': 'mycookie'
+            }
+        })
+        .then((response)=>{
+            expect(response.status).to.eq(200)
+            expect(response.body).has.length(1)
+        })
+    })
 })
